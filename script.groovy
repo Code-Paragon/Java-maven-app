615c0d9d3449d5d996d64cb8c2af649c82d4dfc7
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2024-11-03 13:14:59 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,21 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo "building the application"
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo "Testing the application..."
+def buildDeployImage() {
+    echo "building the docker image"
+    withCredentials([usernamePassword(credentialsId: 'docker_hub_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t retr0009/demo-app:jma-2.1 . '
+        sh '''
+        echo "$PASS" | docker login -u "$USER" --password-stdin
+        '''
+        sh 'docker push retr0009/demo-app:jma-2.1'
+    }
 }
 
 def deployApp() {
     echo "Deploying the application..."
-    echo "Deploying version ${params.VERSION}"
 }
 
 return this
